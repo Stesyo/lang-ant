176a43edb24d2729b24abe35f2eca0a7fba93f1e
Stesyo <stesiox@gmail.com>
2024-01-18 06:38:22 +0100

    display_write wo funkcja lokalna

--- src/display.h
+++ src/display.h
@@ -8,8 +8,6 @@ struct Field field_load(FILE *file_state);
 
 void display_print(struct Field *field, int iteration);
 
-void field_write(struct Field *field, FILE *stream);
-
 void display_save(struct Field *field, int iteration, char *file_out);
 
 #endif // !DISPLAY_H
